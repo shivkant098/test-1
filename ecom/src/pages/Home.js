d4657cf8c67dbdaa085d0011aedf40d2b0c0ce4a
shivkant098 <148935302+shivkant098@users.noreply.github.com>
2023-11-13 02:27:57 +0000

    1

--- ecom/src/pages/Home.js
+++ ecom/src/pages/Home.js
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import "./home.css";
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div className='banner'>
+        <div className='banner-pic'>
+          <img src='https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2021/03/organic-products-hero.png' alt="banner" />
+        </div>
+        <div className='banner-text'>
+          <h5>Best Quality Products</h5>
+          <h1>Join The Organic<br /> Movement!
+          </h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+
+          </p>
+          <button className='button1'>Shop Now</button>
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
