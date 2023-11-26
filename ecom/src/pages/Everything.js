1de50b3e894e15b436961fc44ccd2ec8665fc1a7
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2023-11-26 18:22:36 +0000

    1

--- ecom/src/pages/Everything.js
+++ ecom/src/pages/Everything.js
@@ -2,17 +2,11 @@ import { useState } from "react";
 import Navigation from "./Shop/Navigation/Nav";
 import Card from "./Shop/Card";
 import Recommended from "./Shop/Recommended/Recommended";
-// import Navigation
 import Products from "./Shop/Products/Products";
-// import Card from "../components/Shop/Card";
-// 
 import Sidebar from "./Shop/Sidebar";
-
 
 const Everything = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
-
-  // ----------- Input Filter -----------
   const [query, setQuery] = useState("");
 
   const handleInputChange = (event) => {
@@ -23,12 +17,10 @@ const Everything = () => {
     (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
   );
 
-  // ----------- Radio Filtering -----------
   const handleChange = (event) => {
     setSelectedCategory(event.target.value);
   };
 
-  // ------------ Button Filtering -----------
   const handleClick = (event) => {
     setSelectedCategory(event.target.value);
   };
@@ -36,12 +28,10 @@ const Everything = () => {
   function filteredData(products, selected, query) {
     let filteredProducts = products;
 
-    // Filtering Input Items
     if (query) {
       filteredProducts = filteredItems;
     }
 
-    // Applying selected filter
     if (selected) {
       filteredProducts = filteredProducts.filter(
         ({ category, color, company, newPrice, title }) =>
@@ -53,35 +43,30 @@ const Everything = () => {
       );
     }
 
-    return filteredProducts.map(
-      ({ img, title, star, reviews, prevPrice, newPrice }) => (
-        <Card
-          key={Math.random()}
-          img={img}
-          title={title}
-          star={star}
-          reviews={reviews}
-          prevPrice={prevPrice}
-          newPrice={newPrice}
-        />
-      )
-    );
+    return filteredProducts.map(({ img, title, star, reviews, prevPrice, newPrice }) => (
+      <Card
+        key={title} // Use a unique identifier from your data as the key
+        img={img}
+        title={title}
+        star={star}
+        reviews={reviews}
+        prevPrice={prevPrice}
+        newPrice={newPrice}
+      />
+    ));
   }
 
   const result = filteredData(Products, selectedCategory, query);
 
   return (
     <>
-    <h1>hiii</h1>
+      <h1>hiii</h1>
       <Sidebar handleChange={handleChange} />
       <Navigation query={query} handleInputChange={handleInputChange} />
       <Recommended handleClick={handleClick} />
       <Products result={result} />
     </>
   );
-}
+};
 
-// export default App;
-
-
-export default Everything
+export default Everything;
