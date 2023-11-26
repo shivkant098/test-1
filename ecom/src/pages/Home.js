cea646fef505d359cc9bc70572f71dc5c0d8f295
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2023-11-26 16:56:30 +0000

    1

--- ecom/src/pages/Home.js
+++ ecom/src/pages/Home.js
@@ -15,7 +15,13 @@ const Home = () => {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
 
           </p>
-          <button className='button1'>Shop Now</button>
+          <div className='button-container'>
+            <button className='cta-button'>
+              <i className="fas fa-shopping-cart cart-btn"></i> Shop Now
+            </button>
+          </div>
+
+          {/* <button className='button1'>Shop Now</button> */}
         </div>
       </div>
       <div className='features'>
@@ -123,13 +129,187 @@ const Home = () => {
         </div>
 
       </div>
+
+
+      {/* featureProducts */}
       <img src='https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2021/03/basil-leaf.png' alt="im" className='mid-img' align="center" />
 
       <div className="other-products">
-        
-        <FeatureProduct/>
-        
+
+        <FeatureProduct />
+
+      </div>
+      <div className='banner1'>
+        <div className='banner-content'>
+          <div className='text-container'>
+            <h2 className='banner-heading'>Get 25% Off On Your First Purchase!</h2>
+          </div>
+          <div className='button-container'>
+            <button className='cta-button'>
+              <i className="fas fa-shopping-cart cart-btn"></i> Shop Now
+            </button>
+          </div>
         </div>
+      </div>
+
+      {/* trending product */}
+      <div className='content'>
+        <div className='text1'>
+          <h2>Trending Products
+          </h2>
+          <br />
+          <img src='https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2019/07/logo-leaf-new.png' alt='imga' />
+        </div>
+      </div>
+
+      <div className='product'>
+        <div className='product-item1'>
+          <div className='product-main'>
+            <img src="https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/orage-juice-kariz-300x300.jpg" alt="imgn" />
+            <p>Groceries</p>
+            <h2>Assorted coffee</h2>
+            {/* </div> */}
+            <div className='rating'>
+              <span className="fa fa-star checked"></span>
+              <span className="fa fa-star checked"></span>
+              <span className="fa fa-star checked"></span>
+              <span className="fa fa-star"></span>
+              <span className="fa fa-star"></span>
+            </div>
+          </div>
+        </div>
+
+        <div className='product-item2'>
+          <div className='product-main'>
+            <img src="https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/sanitizer-300x300.jpg" alt='product-pic' />
+            <p>Groceries</p>
+            <h2>Assorted coffee</h2>
+          </div>
+          <div className='rating'>
+            <span className="fa fa-star checked"></span>
+            <span className="fa fa-star checked"></span>
+            <span className="fa fa-star checked"></span>
+            <span className="fa fa-star"></span>
+            <span className="fa fa-star"></span>
+          </div>
+        </div>
+        <div className='product-item3'>
+          <div className='product-main'>
+            <img src='https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/red-chillies-300x300.jpg' alt="im" />
+            <p>Groceries</p>
+            <h2>Assorted coffee</h2>
+          </div>
+          <div className='rating'>
+            <span className="fa fa-star checked"></span>
+            <span className="fa fa-star checked"></span>
+            <span className="fa fa-star checked"></span>
+            <span className="fa fa-star"></span>
+            <span className="fa fa-star"></span>
+          </div>
+        </div>
+
+        <div className='product-item4'>
+          <div className='product-main'>
+            <span className='sale'>Sale!</span>
+            <img src="https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2018/06/edible-oil-300x300.jpg" alt="im" />
+            <p>Groceries</p>
+            <h2>Assorted coffee</h2>
+          </div>
+          <div className='rating'>
+            <span className="fa fa-star checked"></span>
+            <span className="fa fa-star checked"></span>
+            <span className="fa fa-star checked"></span>
+            <span className="fa fa-star"></span>
+            <span className="fa fa-star"></span>
+          </div>
+        </div>
+
+      </div>
+
+      
+      <section id="testimonials">
+        <div class="testimonial-heading">
+            <span>Comments</span>
+            <h1>Clients Says</h1>
+        </div>
+        <div class="testimonial-box-container">
+            <div class="testimonial-box">
+                <div class="box-top">
+                    <div class="profile">
+                        <div class="profile-img">
+                            <img src="images/c-1.jpg" />
+                        </div>
+                        <div class="name-user">
+                            <strong>Touseeq Ijaz</strong>
+                            <span>@touseeqijazweb</span>
+                        </div>
+                    </div>
+                    <div class="reviews">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="far fa-star"></i><!--Empty star-->
+                    </div>
+                </div>
+                <div class="client-comment">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat quis? Provident temporibus architecto asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam tenetur voluptates incidunt blanditiis sed atque cumque.</p>
+                </div>
+            </div>
+            <div class="testimonial-box">
+                <div class="box-top">
+                    <div class="profile">
+                        <div class="profile-img">
+                            <img src="images/c-2.jpg" />
+                        </div>
+                        <div class="name-user">
+                            <strong>J.K Rowling</strong>
+                            <span>@jkrowling</span>
+                        </div>
+                    </div>
+                    <div class="reviews">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i><!--Empty star-->
+                    </div>
+                </div>
+                <div class="client-comment">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat quis? Provident temporibus architecto asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam tenetur voluptates incidunt blanditiis sed atque cumque.</p>
+                </div>
+            </div>
+            <div class="testimonial-box">
+                <div class="box-top">
+                    <div class="profile">
+                        <div class="profile-img">
+                            <img src="images/c-3.jpg" />
+                        </div>
+                        <!--name-and-username-->
+                        <div class="name-user">
+                            <strong>Harry Potter</strong>
+                            <span>@DanielRedclief</span>
+                        </div>
+                    </div>
+                    <!--reviews------>
+                    <div class="reviews">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="far fa-star"></i><!--Empty star-->
+                    </div>
+                </div>
+                <!--Comments---------------------------------------->
+                <div class="client-comment">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, quaerat quis? Provident temporibus architecto asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam tenetur voluptates incidunt blanditiis sed atque cumque.</p>
+                </div>
+            </div>
+            <!--BOX-4-------------->
+            <div class="testimonial-box">
+                <!--top------------------------->
+     
+
 
 
 
