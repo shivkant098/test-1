ed53c081d1a8a7e36a3124c4c944bcd324a80cd3
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2023-11-26 17:50:23 +0000

    1

--- ecom/src/pages/Everything.js
+++ ecom/src/pages/Everything.js
@@ -1,9 +1,87 @@
-import React from 'react'
+import { useState } from "react";
+import Navigation from "./Shop/Navigation/Nav";
+import Card from "./Shop/Card";
+import Recommended from "./Shop/Recommended/Recommended";
+// import Navigation
+import Products from "./Shop/Products/Products";
+// import Card from "../components/Shop/Card";
+// 
+import Sidebar from "./Shop/Sidebar";
+
 
 const Everything = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  // ----------- Input Filter -----------
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const filteredItems = Products.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
+
+  // ----------- Radio Filtering -----------
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  // ------------ Button Filtering -----------
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
+
+    // Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    // Applying selected filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
+  }
+
+  const result = filteredData(Products, selectedCategory, query);
+
   return (
-    <div>Everything</div>
-  )
+    <>
+    <h1>hiii</h1>
+      <Sidebar handleChange={handleChange} />
+      <Navigation query={query} handleInputChange={handleInputChange} />
+      <Recommended handleClick={handleClick} />
+      <Products result={result} />
+    </>
+  );
 }
+
+// export default App;
+
 
 export default Everything
