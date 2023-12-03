fe2368c7434ce42e066be7d0945d62c50e7c0c5e
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2023-12-03 16:25:10 +0000

    1

--- ecom/src/reducer/productReducer.js
+++ ecom/src/reducer/productReducer.js
@@ -1,31 +1,43 @@
 // import React from 'react';
 
 const ProductReducer = (state, action) => {
-    switch (action.type) {
-      case 'loading':
-        return {
-          ...state,
-          isLoading: true,
-        }; 
-      case 'set_api_data':
-        const featureData = action.payload.filter((curElem) => {
-          return curElem.featured === true;
-        });
-        return {
-          ...state,
-          isLoading: false,
-          products: action.payload,
-          featureProducts: featureData,
-        };
-      case 'api_error':
-        return {
-          ...state,
-          isLoading: false,
-          isError: true,
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default ProductReducer;
+  switch (action.type) {
+    case 'set_single_loading':
+      return {
+        ...state,
+        isSingleLoading: true,
+      };
+    case 'set_api_data':
+      const featureData = action.payload.filter((curElem) => {
+        return curElem.featured === true;
+      });
+      return {
+        ...state,
+        isLoading: false,
+        products: action.payload,
+        featureProducts: featureData,
+      };
+    case 'set_single_product':
+      return {
+        ...state,
+        isSingleLoading: false,
+        singleProduct: action.payload,
+      };
+    case 'single_error':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case 'api_error':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export default ProductReducer;
